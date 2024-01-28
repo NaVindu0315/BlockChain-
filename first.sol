a186cd73a8ea2a01b089747a2f87369e43f3bd44
NaVindu0315 <navindulakshan99@gmail.com>
2024-01-28 19:37:44 +0530

    added get and set functions

--- first.sol
+++ first.sol
@@ -1,1 +1,31 @@
-hi
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.8.2 <0.9.0;
+
+/**
+ * @title Storage
+ * @dev Store & retrieve value in a variable
+ * @custom:dev-run-script ./scripts/deploy_with_ethers.ts
+ */
+
+ contract MyContract{
+    string value;
+    
+
+
+
+     
+   function get() public view  returns(string memory)
+   {
+      return value;
+
+   }
+
+   function set(string memory _value) public 
+
+   {
+      value = _value;
+
+   } 
+
+ }
